c72160ad039f6c3933e1c832ee2de54d22b669b2
randi3281 <randiafif2312@gmail.com>
2023-08-18 06:23:38 +0700

    captcha

--- public/captcha/captcha.php
+++ public/captcha/captcha.php
@@ -3,7 +3,8 @@ session_start();
 
 header("Content-type: image/png");
 
-$_SESSION['Captcha'] = "";
+// buatlah variabel session menyimpan captcha yang tersimpan di daftarproses di anfiniticontroller
+session('Captcha', '');
 
 $gbr = imagecreate(200, 50);
 imagecolorallocate($gbr, 69, 179, 157);
@@ -12,12 +13,10 @@ $color = imagecolorallocate($gbr, 253, 252, 252);
 $font = "./Allura-Regular.otf";
 $ukuran_font = 20;
 $posisi = 32;
-// $angka2 = "";
 
 for ($i = 0; $i <= 5; $i++) {
     $angka = rand(0, 9);
-    // $angka2 .= $angka;
-    $_SESSION['Captcha'] .= $angka;
+    session('Captcha', session('Captcha') . $angka);
 
     $kemiringan = rand(20, 20);
 
@@ -25,6 +24,7 @@ for ($i = 0; $i <= 5; $i++) {
 
 
 }
+
 // $host = mysqli_connect('localhost', 'randigro_main', 'Randi_328', 'randigro_main');
 // mysqli_query($host, 'INSERT INTO `accaptcha` (`captcha`) VALUES ('.$angka2.')' );
 imagepng($gbr);
