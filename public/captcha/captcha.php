58d8291afdf117f80bdf9732a58f5aac0fbefa07
randi3281 <randiafif2312@gmail.com>
2023-08-18 06:37:37 +0700

    save

--- public/captcha/captcha.php
+++ public/captcha/captcha.php
@@ -1,31 +1,18 @@
+<!-- buatlah kode captcha untuk ukuran yang pas di foler anficititate/login.blade.php dengan kode php-->
 <?php
 session_start();
+$random = rand(1000, 9999);
+$_SESSION['captcha'] = $random;
+// buatlah $random menjadi gambar captcha
+$im = imagecreatetruecolor(50, 24);
+$bg = imagecolorallocate($im, 22, 86, 165);
+$fg = imagecolorallocate($im, 255, 255, 255);
+imagefill($im, 0, 0, $bg);
+imagestring($im, 5, 5, 5,  $random, $fg);
+header("Cache-Control: no-cache, must-revalidate");
+header('Content-type: image/png');
+imagepng($im);
 
-header("Content-type: image/png");
+imagedestroy($im);
 
-$_SESSION['Captcha'] = "";
-
-$gbr = imagecreate(200, 50);
-imagecolorallocate($gbr, 69, 179, 157);
-
-$color = imagecolorallocate($gbr, 253, 252, 252);
-$font = "./Allura-Regular.otf";
-$ukuran_font = 20;
-$posisi = 32;
-// $angka2 = "";
-
-for ($i = 0; $i <= 5; $i++) {
-    $angka = rand(0, 9);
-    // $angka2 .= $angka;
-    $_SESSION['Captcha'] .= $angka;
-
-    $kemiringan = rand(20, 20);
-
-    imagettftext($gbr, $ukuran_font, $kemiringan, 8 + 15 * $i, $posisi, $color, $font, $angka);
-
-
-}
-// $host = mysqli_connect('localhost', 'randigro_main', 'Randi_328', 'randigro_main');
-// mysqli_query($host, 'INSERT INTO `accaptcha` (`captcha`) VALUES ('.$angka2.')' );
-imagepng($gbr);
-imagedestroy($gbr);
+?>
