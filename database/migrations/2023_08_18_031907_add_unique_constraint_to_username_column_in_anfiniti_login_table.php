2d1bbc43473723d2dacea6560ac3ed4e1ab06e9a
randi3281 <randiafif2312@gmail.com>
2023-08-18 10:19:48 +0700

    save

---
+++ database/migrations/2023_08_18_031907_add_unique_constraint_to_username_column_in_anfiniti_login_table.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqueConstraintToUsernameColumnInAnfinitiLoginTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('anfiniti_login', function (Blueprint $table) {
+            $table->unique('username');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('anfiniti_login', function (Blueprint $table) {
+            $table->dropUnique('anfiniti_login_username_unique');
+        });
+    }
+}
