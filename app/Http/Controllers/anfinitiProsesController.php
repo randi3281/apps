62a6d083b33c6a3e14ab6bda4d44072dc8473456
randi3281 <randiafif2312@gmail.com>
2023-08-19 11:30:15 +0700

    salahcaptcha

--- app/Http/Controllers/anfinitiProsesController.php
+++ app/Http/Controllers/anfinitiProsesController.php
@@ -60,37 +60,39 @@ class anfinitiProsesController extends Controller
             ]);
 
             $anfinitiLogin = anfiniti_login::where("username", $validatedData['username'])->first();
-            if($anfinitiLogin){                
-                if(password_verify($validatedData['password'], $anfinitiLogin->password)){
-                    // saya ingin membuat sebuah variabel yang berisi angka 100 digit tapi acak
-                    $random = Str::random(100);
-                    // enkripsi $random dan masukkan ke database anfiniti_session yang mana ada login_id dan sesi
-                    $anfinitiSession = new anfiniti_session;
-                    $anfinitiSession->login_id = $anfinitiLogin->id;
-                    $anfinitiSession->sesi = $random;
-                    $anfinitiSession->username = bcrypt($validatedData['username']);
-                    $anfinitiSession->save();
-                    // set cookie selama 360 hari
-                    
-                    $data = [
-                        'tokennya' => $random,
-                        'username' => $validatedData['username']
-                    ];
-                    
-                    // Mengenkripsi data sebelum menyimpannya dalam cookie
-                    $dataEncrypted = encrypt($data);
-                    $response = new Response('Data disimpan dalam cookie');
-                    $response->cookie("anfiniti_sessionnya", $dataEncrypted, time() + (86400 * 360), "/");
+            if($anfinitiLogin){
+                if($validatedData['captcha'] == $_SESSION['Captcha']){                
+                    if(password_verify($validatedData['password'], $anfinitiLogin->password)){
+                        // saya ingin membuat sebuah variabel yang berisi angka 100 digit tapi acak
+                        $random = Str::random(100);
+                        // enkripsi $random dan masukkan ke database anfiniti_session yang mana ada login_id dan sesi
+                        $anfinitiSession = new anfiniti_session;
+                        $anfinitiSession->login_id = $anfinitiLogin->id;
+                        $anfinitiSession->sesi = $random;
+                        $anfinitiSession->username = bcrypt($validatedData['username']);
+                        $anfinitiSession->save();
+                        // set cookie selama 360 hari
+                        
+                        $data = [
+                            'tokennya' => $random,
+                            'username' => $validatedData['username']
+                        ];
+                        
+                        // Mengenkripsi data sebelum menyimpannya dalam cookie
+                        $dataEncrypted = encrypt($data);
+                        $response = new Response('Data disimpan dalam cookie');
+                        $response->cookie("anfiniti_sessionnya", $dataEncrypted, time() + (86400 * 360), "/");
 
-                    // Membuat cookie dengan nama 'data_user' dan nilai $dataEncrypted
+                        // Membuat cookie dengan nama 'data_user' dan nilai $dataEncrypted
 
 
-                    return redirect()->route("anfiniti");
+                        return redirect()->route("anfiniti");
+                    }else{
+                        return redirect("/anfiniti/login/1");
+                    }
                 }else{
-                    return redirect("/anfiniti/login/1");
+                    return redirect("/anfiniti/login/2");
                 }
-            }else{
-                return redirect("/anfiniti/login/1");
             }
         };
 
