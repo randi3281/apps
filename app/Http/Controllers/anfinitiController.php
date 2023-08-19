bf6a7cf367224e79996262edffe140dacc787b7e
randi3281 <randiafif2312@gmail.com>
2023-08-19 15:55:28 +0700

    daa

--- app/Http/Controllers/anfinitiController.php
+++ app/Http/Controllers/anfinitiController.php
@@ -52,9 +52,9 @@ class anfinitiController extends Controller
             if($anfinitiSession){
                 if(password_verify($username, $anfinitiSession->username)){
                     // buatlah  fungsi untuk mengambil data dari database anfiniti_dataweb berdasarkan login_id yang didapat dari anfiniti_session dengan urut dari id
-                    $anfinitiDataweb = anfiniti_dataweb::where("login_id", $login_id)->orderBy('id', 'asc')->get();
-                    if($anfinitiDataweb){
-                        return view("anfinitiView.anfiniti", compact($anfinitiDataweb));
+                    $dataweb = anfiniti_dataweb::where("login_id", $login_id)->orderBy('id', 'asc')->get();
+                    if($dataweb){
+                        return view("anfinitiView.anfiniti", compact($dataweb));
                     }else{
                         return view("anfinitiView.anfiniti");
                     };
--- resources/views/anfinitiView/anfiniti.blade.php
+++ resources/views/anfinitiView/anfiniti.blade.php
@@ -28,8 +28,8 @@
         @endphp
         <div class="row justify-content-center mt-5">
             
-            {{-- @foreach ($anfinitiDataweb as $data) --}}
-                {{-- <div class="card custom-card mx-2" style="width: 147px; height: 147px">
+            @foreach ($dataweb as $data)
+                <div class="card custom-card mx-2" style="width: 147px; height: 147px">
                     <div class="card-body text-center">
                         <div class="row justify-content-center" style="margin-top:-10px">
                             <div class="col-md-1 d-flex justify-content-center align-items-center mb-4 mx-3">
@@ -62,8 +62,8 @@
                         $y += 7;
                     @endphp
                     <p></p>
-                @endif --}}
-            {{-- @endforeach --}}
+                @endif
+            @endforeach
 
             <div class="card custom-card mx-2" style="width: 147px; height: 147px">
                 <a href="/anfiniti/input" class=" text-decoration-none" style="line-height: 16px">
