9cd242b3b93e71259436f3cc370f132846926157
randi3281 <randiafif2312@gmail.com>
2023-09-13 11:17:39 +0700

    STEKTA

--- resources/views/ukt/index.blade.php
+++ resources/views/ukt/index.blade.php
@@ -4,33 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UKT -/ U . K . T /</title>
+    <title>STEKTA - Sistem Terintegrasi Kewirausahaan Technosantri</title>
+    <link rel="icon" href="/uktPublic/logo.ico" type="image/x-icon">
     <!-- Tambahkan link Bootstrap CSS -->
     @vite('resources/sass/app.scss')
+    <link rel="stylesheet" href="/uktPublic/style.css">
 </head>
 
-<body class="bg-primary">
-    <div class="container mt-5">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-4 mt-5">
-                <div class="card">
-                    <div class="card-header">Form Login</div>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4 justify-content-center align-items-center d-flex" style="height: 640px">
+                <div class="card w-100 custom-shadow">
                     <div class="card-body">
+                        <div class="d-flex justify-content-center disabled">
+                            <img class="img-fluid mt-2 w-25 mb-3 mx-auto" src="/uktPublic/logo.png" alt="">
+                        </div>
+                        <p class="text-center fs-5 fw-bold" style="margin-bottom: -15px"> <span
+                                style="color: #00b4fa">STEK</span><span style="color: #33cc33">TA</span></p>
+                        <hr>
+                        <p class="text-center fs-6 fw-bold" style="margin-bottom: -5px; margin-top: -10px">Sistem
+                            Terintegrasi</p>
+                        <p class="text-center fs-6 fw-bold" style="margin-bottom: 15px">Kewirausahaan Technosantri</p>
                         <form>
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Username</label>
-                                <input type="text" class="form-control" id="username" name="username" required>
+                            <div class="row justify-content-center">
+                                <div class="col-md-10">
+                                    <div class="mb-1 form-group">
+                                        <label for="username" class="form-label">Username</label>
+                                        <input type="text" class="form-control" id="username" name="username"
+                                            placeholder="Masukkan username kamu disini" required autofocus>
+                                    </div>
+                                    <div class="mb-1">
+                                        <label for="password" class="form-label">Password</label>
+                                        <input type="password" class="form-control" id="password" name="password"
+                                            placeholder="Masukkan password kamu disini" required>
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <label style="margin-right: 12px">Captcha</label>
+                                        <img src="/captcha/captcha.php" alt="gambar">
+                                    </div>
+                                    <div class="form-group mt-3">
+                                        <input id="captcha" class="form-control" type="text" name="captcha"
+                                            placeholder="Masukkan captcha di atas kesini" maxlength="6">
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" required>
+                            <div class="text-center mb-4 mt-4">
+                                <button type="submit" class="btn w-50 btn-primary">Login</button>
                             </div>
-                            <div class="mb-3">
-                                <label for="captcha" class="form-label">Captcha</label>
-                                <input type="text" class="form-control" id="captcha" name="captcha" required>
-                                <!-- Anda dapat menambahkan captcha di sini jika diperlukan -->
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
                         </form>
                     </div>
                 </div>
