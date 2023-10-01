fdcae16612627a644fbccb5aa8e0ad0d4f069046
randi3281 <randiafif2312@gmail.com>
2023-10-01 14:20:20 +0700

    tes

--- app/Http/Controllers/FootnoteController.php
+++ app/Http/Controllers/FootnoteController.php
@@ -141,11 +141,10 @@ class FootnoteController extends Controller
                 }else{
                     $volumeJurnal = ConverToRoman($request->volume);
                 }
-                $kataPertama = pecahKataPertama($request->penulis_1);
                 if($_SESSION['jumlahpenulis'] == 3){
                     DB::table('footnote')->insert([
                         'id' => $request->nourut,
-                        'kataPertama' => $kataPertama,
+                        'kataPertama' => pecahKataPertama($request->penulis_1),
                         'penulis_1' => $request->penulis_1,
                         'penulis_2' => $request->penulis_2,
                         'penulis_3' => $request->penulis_3,
@@ -167,7 +166,7 @@ class FootnoteController extends Controller
                 } elseif ($_SESSION['jumlahpenulis'] == 2){
                     DB::table('footnote')->insert([
                         'id' => $request->nourut,
-                        'kataPertama' => $kataPertama,
+                        'kataPertama' => pecahKataPertama($request->penulis_1),
                         'penulis_1' => $request->penulis_1,
                         'penulis_2' => $request->penulis_2,
                         'judul' => $request->judul,
@@ -188,7 +187,7 @@ class FootnoteController extends Controller
                 } elseif ($_SESSION['jumlahpenulis'] == 1 || $_SESSION['jumlahpenulis'] == 4){
                     DB::table('footnote')->insert([
                         'id' => $request->nourut,
-                        'kataPertama' => $kataPertama,
+                        'kataPertama' => pecahKataPertama($request->penulis_1),
                         'penulis_1' => $request->penulis_1,
                         'judul' => $request->judul,
                         'kota' => $request->kota,
