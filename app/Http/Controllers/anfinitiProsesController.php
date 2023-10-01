240efb2fb0fc6d3f086a33e1dc7813835e9c8153
randi3281 <randiafif2312@gmail.com>
2023-10-01 09:23:22 +0700

    koment

--- app/Http/Controllers/anfinitiProsesController.php
+++ app/Http/Controllers/anfinitiProsesController.php
@@ -222,8 +222,8 @@ class anfinitiProsesController extends Controller
         if(isset($request->tombolHapusSelamanya)){
             // hapus selamanya yang mempunyai login_id yang sama dengan login_id yang sedang login dan idnya dalam onlytrashed
             $anfinitiDataweb = anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->where("id", $request->idData)->first();
-            // $namaGambar = $anfinitiDataweb->gambar;
-            // unlink(public_path('anfinitiPublic/images/'.$namaGambar));
+            $namaGambar = $anfinitiDataweb->gambar;
+            unlink(public_path('anfinitiPublic/images/'.$namaGambar));
             anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->where("id", $request->idData)->forceDelete();
             // hapus juga file gambar yang ada di folder public
             
@@ -232,12 +232,12 @@ class anfinitiProsesController extends Controller
         
         if(isset($request->tombolHapusSemuaSelamanya)){
             // hapus selamanya yang mempunyai login_id yang sama dengan login_id yang sedang login dan idnya dalam onlytrashed
-            $anfinitiDataweb = anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->get();
-            // foreach($anfinitiDataweb as $data){
-            //     $namaGambar = $data->gambar;
-            //     unlink(public_path('anfinitiPublic/images/'.$namaGambar));
-            // };
-            anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->forceDelete();
+            // $anfinitiDataweb = anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->get();
+            // // foreach($anfinitiDataweb as $data){
+            // //     $namaGambar = $data->gambar;
+            // //     unlink(public_path('anfinitiPublic/images/'.$namaGambar));
+            // // };
+            // anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->forceDelete();
             return redirect()->route("trashAnfiniti");            
         };
 
