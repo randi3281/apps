e974a5c0c6a5ef302d0cb5aaaf2a6fea6abb1753
randi3281 <randiafif2312@gmail.com>
2023-10-01 09:24:07 +0700

    okefix

--- app/Http/Controllers/anfinitiProsesController.php
+++ app/Http/Controllers/anfinitiProsesController.php
@@ -233,10 +233,10 @@ class anfinitiProsesController extends Controller
         if(isset($request->tombolHapusSemuaSelamanya)){
             // hapus selamanya yang mempunyai login_id yang sama dengan login_id yang sedang login dan idnya dalam onlytrashed
             $anfinitiDataweb = anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->get();
-            // foreach($anfinitiDataweb as $data){
-            //     $namaGambar = $data->gambar;
-            //     unlink(public_path('anfinitiPublic/images/'.$namaGambar));
-            // };
+            foreach($anfinitiDataweb as $data){
+                $namaGambar = $data->gambar;
+                unlink(public_path('anfinitiPublic/images/'.$namaGambar));
+            };
             anfiniti_dataweb::onlyTrashed()->where("login_id", $login_id)->forceDelete();
             return redirect()->route("trashAnfiniti");            
         };
