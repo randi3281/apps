5d34321b015dbcab0a3a969c0731078e242fd310
randi3281 <randi3281@gmail.com>
2023-12-02 10:55:28 +0700

    Fix route namespace in belajarlogin.php

--- routes/belajar/belajarlogin.php
+++ routes/belajar/belajarlogin.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'belajarLoginController@index')->name('belajarlogin');
+Route::get('/', 'belajar\belajarLoginController@index')->name('belajarlogin');
 
 // Route::get('/auth/google', [App\Http\Controllers\Belajar\BelajarLoginController::class, 'redirectToGoogle'])->name('redirect.google');
 // Route::get('/auth/google/callback', [App\Http\Controllers\Belajar\BelajarLoginController::class, 'handleGoogleCallback'])->name('callback.google');
