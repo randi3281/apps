27e9f6311924e15996b6f1e821d697248daf60a3
randi3281 <randi3281@gmail.com>
2023-12-02 11:11:45 +0700

    Fix import statement for Laravel Socialite

--- app/Http/Controllers/belajar/belajarLoginController.php
+++ app/Http/Controllers/belajar/belajarLoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\belajar;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Facades\Socialite;
 
 class belajarLoginController extends Controller
 {
