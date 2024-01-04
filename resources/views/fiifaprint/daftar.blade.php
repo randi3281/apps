88a3c09080affbe6d3747fade1678d12cab0e2f3
randi3281 <randiafif2312@gmail.com>
2024-01-04 20:36:25 +0700

    Update login page and index.blade.php

--- resources/views/fiifaprint/daftar.blade.php
+++ resources/views/fiifaprint/daftar.blade.php
@@ -12,9 +12,9 @@
 
 <body class="bg-primary">
     <div class="container-fluid">
-        <div class="row justify-content-center mt-5">
+        <div class="row justify-content-center mt-4">
             <div class="col-md-4">
-                <div class="card" style="height: 550px">
+                <div class="card" style="height: 600px">
                     <div class="card-body">
                         <div class="text-center">
                             <img src="/fiifaprintpublic/image/icon.png" class="mt-3" style="width: 100px"
@@ -22,7 +22,7 @@
                         </div>
                         <div class="row justify-content-center">
                             <div class="col-md-9 mt-4">
-                                <h3 class="text-center">Login</h3>
+                                <h3 class="text-center">Daftar</h3>
                                 <hr class="mt-3">
                                 <form action="/fiifaprint/login" method="post">
                                     {{ csrf_field() }}
@@ -31,18 +31,23 @@
                                         <input type="text" name="email" class="form-control"
                                             placeholder="Masukkan Email Disini...">
                                     </div>
-                                    <div class="form-group mb-4">
+                                    <div class="form-group mb-2">
                                         <label for="password">Password</label>
                                         <input type="password" name="password" class="form-control"
                                             placeholder="Masukkan Password Disini...">
                                     </div>
+                                    <div class="form-group mb-2">
+                                        <label for="password">Konfirmasi Password</label>
+                                        <input type="password" name="password" class="form-control"
+                                            placeholder="Masukkan Password Disini...">
+                                    </div>
                                     <hr>
-                                    <p class="text-center" style="font-size: 11px">atau kamu mau login atau daftar dengan <a
-                                            href="" class="text-decoration-none text-danger">Google</a></p>
-                                    <div class="text-center mt-4">
-                                        <input type="submit" value="Masuk" name="tombollogin" class="btn btn-primary">
-                                        <input type="submit" value="Daftar" name="tomboldaftar"
-                                            class="btn btn-danger">
+                                    <div class="text-center">
+                                        <input type="submit" value="Daftar" name="tombollogin"
+                                            class="btn btn-primary mb-3">
+                                        <div class="text-center" style="font-size: 11px">atau kamu mau daftar dengan <a href="" class="text-danger text-decoration-none">Google</a></div>
+                                        <p class="text-center" style="font-size: 11px">Sudah punya akun? <a
+                                                href="/fiifaprint" class="text-decoration-none">Login Disini</a></p>
                                     </div>
                                 </form>
                             </div>
