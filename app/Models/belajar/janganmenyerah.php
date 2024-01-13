d4bfa17d9bf25d59fc85ad79b8e4abbb426089cd
randi3281 <randiafif2312@gmail.com>
2024-01-13 21:19:11 +0700

    Updated file permissions and modes

---
+++ app/Models/belajar/janganmenyerah.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\belajar;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class janganmenyerah extends Model
+{
+    use HasFactory;
+}
---
+++ database/seeders/belajar/janganmenyerah.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders\Belajar;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class janganmenyerah extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('id_ID');
+        for ($i = 1; $i <= 10; $i++) {
+            DB::table('janganmenyerahs')->insert([
+                'nama' => $faker->name,
+                'alamat' => $faker->address,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
