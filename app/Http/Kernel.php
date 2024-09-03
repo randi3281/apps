5dbd21cd143fe86ac85d8372faf78937f7506c7f
randi3281 <randi3281@gmail.com>
2024-09-03 20:37:57 +0700

    Update casnitilogin middleware to fix typo in class name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'tesmiddleware' => \App\Http\Middleware\TesMiddleware::class,
-        'casnitilogin' => \App\Http\Middleware\casniti\casnitiLogin::class,
+        'casnitilogin' => \App\Http\Middleware\casniti\casnitilogin::class,
     ];
 }
